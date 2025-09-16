39825b8f5ca97c63ac7c5baa2b37953efbfd6b2c
Hemanthanne411 <hemanth.anne101@gmail.com>
2025-09-16 19:51:42 +0200

    Params.yaml configured

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+import yaml
 
 os.makedirs('logs', exist_ok=True)
 
@@ -30,6 +30,24 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 #type hinting
 def data_load(data_file : str) -> pd.DataFrame:
@@ -77,7 +95,8 @@ def save_data(store_data: str, train_data: pd.DataFrame, test_data: pd.DataFrame
 
 def main():
     try:
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
         data_paths = './experiments/spam.csv'
         df = data_load(data_file=data_paths)
         final_df = clean_data(df)
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
+import yaml
 
 os.makedirs('logs', exist_ok=True)
 
@@ -29,6 +30,23 @@ if not logger.handlers:
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
 
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 # ---------- Data loading ----------
 def load_data(file_path: str) -> pd.DataFrame:
@@ -96,8 +114,8 @@ def save_data(store_data: str, train_data: pd.DataFrame, test_data: pd.DataFrame
 # ---------- Main ----------
 def main():
     try:
-        max_features = 50
-
+        params = load_params(params_path='params.yaml')
+        max_features = params['feature_engineering']['max_features']
         # use load_data (ensures NaN handling)
         train_data = load_data('./data/interim/train_processed.csv')
         test_data = load_data('./data/interim/test_processed.csv')
